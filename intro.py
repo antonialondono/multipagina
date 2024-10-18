5dd2116694514fed49550a4bd2e509e77e75d30b
antonialondono <alondono2@eafit.edu.co>
2024-10-18 14:38:22 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 st.title('Pagina Inicio')
-st.write('Hola! Acá te contaré un poco sobre mi 3 series favoritas de cuando era más pequeña')
+st.write('Hola! Acá te contaré un poco sobre mi serie favorita de cuando era más pequeña')
 
 image = Image.open('series.jpg')
 
