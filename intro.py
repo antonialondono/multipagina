7c47e7c5bfd4df89ae893b93205f1e000a80fbd6
antonialondono <alondono2@eafit.edu.co>
2024-10-18 14:21:58 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from PIL import Image
+
+
 st.title('Pagina Inicio')
 st.write('Hola! Acá te contaré un poco sobre mi 3 series favoritas de cuando era más pequeña')
+
+image = Image.open('series.jpg')
+
+st.image(image,width=300)
+
