ead94aaa0a294d98d57898f3dff6ec5eb0bc7aae
antonialondono <alondono2@eafit.edu.co>
2024-10-18 14:15:34 -0500

    Update pagina1.py

--- pages/pagina1.py
+++ pages/pagina1.py
@@ -1,2 +1,23 @@
 import streamlit as st
-st.title('Pagina 1')
+
+# Título de la página
+st.title('Pagina 1: Información sobre Phineas y Ferb')
+
+# Subtítulo
+st.subheader('¿Qué es Phineas y Ferb?')
+
+# Información sobre el programa
+st.write('''
+**Phineas y Ferb** es una serie de televisión animada creada por Dan Povenmire y Jeff "Swampy" Marsh. 
+Se emitió originalmente en Disney Channel desde 2007 hasta 2015. La serie sigue las aventuras de dos 
+hermanastros, Phineas Flynn y Ferb Fletcher, quienes pasan sus vacaciones de verano creando inventos 
+increíbles y viviendo emocionantes aventuras. Al mismo tiempo, su hermana mayor, Candace, intenta revelar 
+sus travesuras a su madre, aunque siempre falla. 
+
+Paralelamente, la serie también sigue las aventuras de su ornitorrinco mascota, Perry, quien en secreto 
+es un agente secreto luchando contra el malvado Dr. Doofenshmirtz.
+''')
+
+
+
+
