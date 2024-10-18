3a537f551abc542c026bb200dd95228a388745a6
antonialondono <alondono2@eafit.edu.co>
2024-10-18 14:26:16 -0500

    Create Gravity falls.py

---
+++ pages/Gravity falls.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# Título de la página
+st.title('Pagina 1: Información sobre Gravity Falls')
+
+# Subtítulo
+st.subheader('¿Qué es Gravity Falls?')
+
+# Información sobre el programa
+st.write('''
+**Gravity Falls** es una serie animada creada por Alex Hirsch para Disney Channel y Disney XD. 
+La serie sigue a los gemelos Dipper y Mabel Pines, quienes son enviados a pasar el verano con su 
+tío abuelo Stan en la misteriosa ciudad de Gravity Falls. Durante su estancia, los gemelos descubren 
+que la ciudad esconde muchos secretos sobrenaturales y se embarcan en una serie de aventuras mientras 
+intentan desentrañar los misterios locales.
+
+El programa es conocido por su humor, sus personajes entrañables y los elementos de misterio que atraen 
+a una audiencia de todas las edades.
+''')
+
+
+# Datos adicionales
+st.subheader('Datos Curiosos')
+st.write('''
+- Gravity Falls se emitió entre 2012 y 2016, y cuenta con 2 temporadas y 40 episodios.
+- La serie está llena de acertijos y mensajes cifrados que los fanáticos disfrutan descifrando.
+- Dipper lleva siempre consigo un diario misterioso, que resulta clave para resolver muchos de los enigmas de Gravity Falls.
+''')
+
+
