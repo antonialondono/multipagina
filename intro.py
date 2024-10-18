798176e74fcb0fcc2fbe33f8eb358a273589756a
antonialondono <alondono2@eafit.edu.co>
2024-10-18 14:19:39 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,4 +1,3 @@
 import streamlit as st
 st.title('Pagina Inicio')
-st.write('Hola!
-Acá te contaré un poco sobre mi 3 series favoritas de cuando era más pequeña')
+st.write('Hola! Acá te contaré un poco sobre mi 3 series favoritas de cuando era más pequeña')
